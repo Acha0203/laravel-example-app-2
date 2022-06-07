2cb8307860f89aef81e850842a8d2fd69efecf4c
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2022-06-07 15:53:53 +0900

    Complete front-end applications

---
+++ resources/views/components/element/button.blade.php
@@ -0,0 +1,10 @@
+<button
+    type="submit"
+    class="inline-flex justify-center py-2 px-4 border border-transparent
+    shadow-sm text-sm leading-5 font-medium rounded-md text-white bg-blue-500
+    hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2
+    focus:ring-blue-500"
+>
+    {{ $slot }}
+</button>
+
