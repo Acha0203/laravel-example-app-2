6156958dc19d76f8bf0be5db9635f3a68e263e89
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2022-06-08 03:41:36 +0900

    Modifiy the sending emails process with Queue

--- app/Mail/NewUserIntroduction.php
+++ app/Mail/NewUserIntroduction.php
@@ -4,10 +4,11 @@ namespace App\Mail;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NewUserIntroduction extends Mailable
+class NewUserIntroduction extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -19,7 +20,6 @@ class NewUserIntroduction extends Mailable
 
     /**
      * Create a new message instance.
-     *
      * @return void
      */
     public function __construct(User $toUser, User $newUser)
